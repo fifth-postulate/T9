0f62a036e87cfdfc86c9e0fd547404fdf13c9ceb
Daan van Berkel <daan.van.berkel@alliander.com>
2019-11-11 18:51:15 +0100

    correctly format second example as code

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //!
 //! For example, let's say you want to have a Rust related web application and you need a port number for your server.
 //! Looking at the number pad of your phone and figuring out the right buttons to press to get `rust` is hard work.
-//! Instead could use this crate and the following code:
+//! Instead you could use this crate and the following code:
 //!
 //! ```
 //! # extern crate t9;
@@ -18,6 +18,7 @@
 //! If you, like me, tend to forget which word you used to come up with a port number, this crate has got your back.
 //! You just need to create a tree full of candidate words and call `words_at`:
 //!
+//! ```
 //! # extern crate t9;
 //! # use t9::{pad, tree::Tree};
 //! # fn check_that_7878_corresponds_to_rust() {
